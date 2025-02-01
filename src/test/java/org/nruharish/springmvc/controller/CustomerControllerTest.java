28127870e86d18234b0eff2a3a62175df1115ff0
nruharish <bs.harish@gmail.com>
2025-02-01 19:43:52 +0530

    customer tests

---
+++ src/test/java/org/nruharish/springmvc/controller/CustomerControllerTest.java
@@ -0,0 +1,48 @@
+package org.nruharish.springmvc.controller;
+
+import org.junit.jupiter.api.Test;
+import org.nruharish.springmvc.model.Customer;
+import org.nruharish.springmvc.services.CustomerService;
+import org.nruharish.springmvc.services.CustomerServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+@WebMvcTest(CustomerController.class)
+class CustomerControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockitoBean
+    CustomerService customerService;
+    CustomerServiceImpl customerServiceImpl = new CustomerServiceImpl();
+    @Test
+    void listCustomers() {
+    }
+
+    @Test
+    void getCustomerById() throws Exception {
+        Customer customer = customerServiceImpl.listCustomers().get(0);
+        given(customerService.getCustomerById(customer.getId())).willReturn(customer);
+
+        mockMvc.perform(get("/api/v1/customer/" + customer.getId())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect((status().isOk()))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(customer.getId().toString())))
+                .andExpect(jsonPath("$.customerName", is(customer.getCustomerName())));
+
+
+
+
+    }
+}
