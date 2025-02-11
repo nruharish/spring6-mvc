171f49f12dfe7c4b33f1cc2d62640621e1b3b829
nruharish <bs.harish@gmail.com>
2025-02-11 17:18:28 +0530

    refactored again

--- src/main/java/org/nruharish/springmvc/controller/NotFoundException.java
+++ src/main/java/org/nruharish/springmvc/controller/NotFoundException.java
@@ -1,5 +1,9 @@
 package org.nruharish.springmvc.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.NOT_FOUND, reason="Value Not found")
 public class NotFoundException extends RuntimeException {
     public NotFoundException() {
     }
