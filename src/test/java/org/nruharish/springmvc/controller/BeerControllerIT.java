7a411eb2d2257eb1ebc87b42d7b79e383095f321
nruharish <bs.harish@gmail.com>
2025-02-15 18:17:15 +0530

    it1

--- src/test/java/org/nruharish/springmvc/controller/BeerControllerIT.java
+++ src/test/java/org/nruharish/springmvc/controller/BeerControllerIT.java
@@ -5,6 +5,8 @@ import org.nruharish.springmvc.model.BeerDTO;
 import org.nruharish.springmvc.repositories.BeerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -25,6 +27,14 @@ class BeerControllerIT {
         assertThat(dtos.size()).isEqualTo(3);
     }
 
+    @Rollback
+    @Transactional
+    @Test
+    void testEmptyList(){
+        beerRepository.deleteAll();;
+        List<BeerDTO> dtos = beerController.listBeers();
+        assertThat(dtos.size()).isEqualTo(0);
+    }
 
 
 }
